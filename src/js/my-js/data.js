f2971dca8f50c7d86736a91841e9b388ea253ab9
tz7gg <69346690+tz7gg@users.noreply.github.com>
2022-11-03 18:41:46 +0500

    upd

---
+++ src/js/my-js/data.js
@@ -0,0 +1,8 @@
+const data = [
+    { mark: 'audi', model: 'A4', year: '2015', delivery: '21.05.2021-28.02.2022' },
+    { mark: 'УАЗ', model: '2101', year: '1970', delivery: '21.05.2021-28.02.2022' },
+    { mark: 'audi', model: 'A4', year: '2016', delivery: '01.05.2021-21.03.2022' },
+    { mark: 'ВАЗ', model: '2101', year: '1970', delivery: '21.05.2021-28.02.2022' },
+    { mark: 'audi', model: 'A5', year: '2015', delivery: '21.05.2021-28.06.2021' },
+    { mark: 'ВАЗ', model: '2101', year: '1970', delivery: '21.05.2021-28.02.2022' },
+]
--- src/js/my-js/main.js
+++ src/js/my-js/main.js
@@ -3,7 +3,15 @@ window.addEventListener('load', () => {
     const quaresWrap = document.querySelector('.container__quare-items')
     const circleWrap = document.querySelector('.container__circle-wrap')
     const circleItem = document.querySelector('.container__circle-wrap__item')
-    const btn = document.querySelector('.container__circle-wrap__item__text')
+
+    const form = document.querySelector('.container__form')
+    const formWrap = document.querySelector('.container__form__wrap')
+
+    const formBtns = document.querySelectorAll('.container__form__wrap__select__btn')
+
+    const mark = document.querySelector('.mark')
+    const model = document.querySelector('.model')
+    const year = document.querySelector('.year')
 
     quares.forEach(el => {
         setTimeout(() => {
@@ -25,9 +33,68 @@ window.addEventListener('load', () => {
     }, 6000)
 
     setTimeout(() => {
-        btn.onclick = () => {
-            console.log('dada');
+        circleItem.onclick = () => {
+            form.classList.remove('dn')
+            setTimeout(() => {
+                formWrap.classList.add('active')
+            }, 50)
         }
     }, 7000)
 
+    formBtns.forEach(function(el) {
+        const parent = el.parentElement
+        el.onclick = () => {
+            const wrap = parent.querySelector('.container__form__wrap__select__wrap')
+            const items = wrap.querySelectorAll('.container__form__wrap__select__wrap__item')
+            const btn = parent.querySelector('.container__form__wrap__select__btn')
+            wrap.classList.toggle('dn')
+            items.forEach((el) => {
+                el.onclick = () => {
+                    wrap.classList.add('dn')
+                    btn.innerHTML = el.innerHTML
+                    if (parent.classList.contains('mark')) {
+                        model.classList.remove('disabled')
+                        mark.classList.add('disabled')
+                        fillListType('model', 'mark', data, el.innerHTML)
+                    } else if (parent.classList.contains('model')) {
+                        year.classList.remove('disabled')
+                        model.classList.add('disabled')
+                        fillListType('year', 'model', data, el.innerHTML)
+                    } else if (!parent.classList.contains('model')) {
+                        year.classList.add('disabled')
+                    }
+                }
+            })
+        }
+
+    })
+
+    function fillListType(type, filterType, data, filterText) {
+        let tempData = JSON.stringify(data)
+        tempData = JSON.parse(tempData)
+
+        if (filterText == "init") {
+            tempData = tempData.map((el) => {
+                return el[filterType]
+            })
+        } else {
+            tempData = tempData.filter((el) => {
+                return el[filterType] == filterText
+            }).map((el) => el[type])
+        }
+
+        tempData = Array.from(new Set(tempData))
+
+        const category = document.querySelector(`.${type}`)
+        const itemsWrap = category.querySelector('.container__form__wrap__select__wrap')
+        itemsWrap.innerHTML = ''
+        tempData.forEach(el => {
+            const item = document.createElement('div')
+            item.classList.add('container__form__wrap__select__wrap__item')
+            item.innerText = el
+            itemsWrap.appendChild(item)
+        })
+    }
+
+    fillListType('mark', 'mark', data, 'init')
 })
